e54a4a1571eb0a24d365dc87609e03d38b727a78
Natzsmart <iniobongeffiong40@gmail.com>
2023-04-04 00:33:53 +0100

    files made executable

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -11,7 +11,7 @@ async def wait_random(max_delay: int = 10) -> float:
     ''' wait_random waits for a random delay between 0 and max_delay 10 
     Returns float: random float number.
     '''
-
+    
     random_wait_time = random.random() * max_delay
     await asyncio.sleep(random_wait_time)
     return random_wait_time
