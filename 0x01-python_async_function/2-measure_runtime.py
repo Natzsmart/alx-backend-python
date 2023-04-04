2417eb9cd3414a1e8d4900ead19264c75f065be2
Natzsmart <iniobongeffiong40@gmail.com>
2023-04-04 01:53:20 +0100

    task 2 done

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -11,6 +11,6 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 def measure_time(n: int, max_delay: int) -> float:
     '''determine the usual runtime of wait_n.
     '''
-    start_time = time.time()
+    start_counter = time.time()
     asyncio.run(wait_n(n, max_delay))
-    return (time.time() - start_time) / n
+    return (time.time() - start_counter) / n
