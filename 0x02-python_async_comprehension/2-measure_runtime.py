d92f30fa192cb86729fc15963a27f58236c72c21
Smartino <Iniobongeffiong40@gmail.com>
2023-04-04 23:56:56 -0700

    removed rename module

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -3,15 +3,15 @@
 '''
 import asyncio
 import time
-from importlib import import_module as use
+from importlib import import_module
 
 
-async_comprehension = use('1-async_comprehension').async_comprehension
+async_comprehension = import_module('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
     '''This function executes async_comprehension() 4 times and measures the total runtime.
     '''
-    start_times = time.time()
+    start = time.time()
     await asyncio.gather(*(async_comprehension() for h in range(4)))
-    return time.time() - start_times
+    return time.time() - start
