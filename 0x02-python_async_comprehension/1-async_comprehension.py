5747dcef02341296ab9f63a423ea4c412f7cda4f
Smartino <Iniobongeffiong40@gmail.com>
2023-04-05 00:08:26 -0700

    Task pushed

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -11,4 +11,4 @@ async_generator = use('0-async_generator').async_generator
 async def async_comprehension() -> List[float]:
     '''This function creates a list of 10 floats generated asynchronously from async_generator().
     '''
-    return [numb async for numb in async_generator()]i
+    return [numb async for numb in async_generator()]
