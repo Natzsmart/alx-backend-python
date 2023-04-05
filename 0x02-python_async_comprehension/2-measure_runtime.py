c81e84d8dc23d33a8fd87be68c404a13cf9d874a
Smartino <Iniobongeffiong40@gmail.com>
2023-04-05 01:56:25 -0700

    pycode changes

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -13,9 +13,8 @@ async_comprehension = put('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     '''
-    This function executes async_comprehension() 4 times
+    Executes async_comprehension() 4 times using asyncio.gather
     '''
     start = time.time()
     await asyncio.gather(*(async_comprehension() for h in range(4)))
     return time.time() - start
-
