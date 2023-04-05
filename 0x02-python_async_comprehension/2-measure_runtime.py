608422b010ce4bec4f8e04643bc8b9318a57592e
Smartino <Iniobongeffiong40@gmail.com>
2023-04-05 01:43:08 -0700

    still trying to pass the pycode test

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -14,7 +14,6 @@ async_comprehension = put('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     '''
     This function executes async_comprehension() 4 times
-    and measures the total runtime.
     '''
     start = time.time()
     await asyncio.gather(*(async_comprehension() for h in range(4)))
