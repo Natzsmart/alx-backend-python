71115da0dd16bc282075e53c04fe23c52c90ca36
Smartino <Iniobongeffiong40@gmail.com>
2023-04-04 22:19:07 -0700

    removed whitspacing

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
-
+'''This is the solution to Task 2 module.
 '''
-This is the solution to Task 2 module.
-'''
-
 import asyncio
 import time
 from importlib import import_module
@@ -12,10 +9,9 @@ async_comprehension = import_module('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    ''' This function executes async_comprehension() 4 times and measures the total runtime.
     '''
-    This function executes async_comprehension() 4 times and measures the total runtime.
-    '''
-    start = time.time()
+    start_t = time.time()
     await asyncio.gather(*(async_comprehension() for h in range(4)))
-    return time.time() - start
+    return time.time() - start_t
 
